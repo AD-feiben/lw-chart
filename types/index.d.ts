26dd9ed4abde5cacba6696f9fcfd32068b9c2d9d
ShiRunPei <13016001210@163.com>
2020-05-11 12:43:59 +0800

    update export

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,5 @@
 import LWChart from './lw-chart';
 import Axis from './axis';
 import Area from './area';
-declare const _default: {
-    LWChart: typeof LWChart;
-    Axis: typeof Axis;
-    Area: typeof Area;
-};
-export default _default;
+export default LWChart;
+export { Axis, Area };
